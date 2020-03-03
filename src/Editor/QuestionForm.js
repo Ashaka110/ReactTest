a3bc73af8b94f5133f4a70cfbd06642bc18306a9
Ashaka110 <aahietanen@gmail.com>
2020-03-03 14:55:51 -0500

    QuestionForm Cleanup

--- src/Editor/QuestionForm.js
+++ src/Editor/QuestionForm.js
@@ -4,55 +4,11 @@ const QuestionForm = (props) =>{
 
     const quest = props.Question;
 
-    console.log("quest");
-    console.log(quest);
-
-    const [questionNum, setQuestionNum] = useState('')
-    const [Question, setQuestion] = useState('')
-    const [A, setA] = useState('')
-    const [B, setB] = useState('')
-    const [C, setC] = useState('')
-    const [D, setD] = useState('')
-    const [Aans, setAans] = useState(false)
-    const [Bans, setBans] = useState(false)
-    const [Cans, setCans] = useState(false)
-    const [Dans, setDans] = useState(false)
-
-    function changeValue(e){
-
-        let param = e.currentTarget.id;
-
-        setA(true);
-        props.setQuestions(prev =>{
-
-        let q = prev;
-        console.log(q[props.Num][param]);
-        q[props.Num][param] = e.currentTarget.value;
-        console.log(q[props.Num][param]);
-
-        return q;
-        });
-    }
-
-    function onUpdate (){
-        const up = ({
-            Question,
-            A,
-            B,
-            C,
-            D,
-            Aans,
-            Bans,
-            Cans,
-            Dans,
-        })        
-        props.Update(up);
-    }
     function field (id){
-        return <input type="text" id={id} value={props.Questions[props.Num][id]} onChange={e => {props.Update(id, e.currentTarget.value )}} />
+        return  <input type="text" id={id} value={props.Questions[props.Num][id]} onChange={e => {props.Update(id, e.currentTarget.value )}} />
     }
     function checkbox (id){
-        return <input type="checkbox" checked={quest[id]} onChange={e => { props.Update(id, e.currentTarget.checked) }} /> <span></span>
+        return <><input type="checkbox" checked={quest[id]} onChange={e => { props.Update(id, e.currentTarget.checked) }} /> <span></span></>
     }
     function answerField (id){
         return(
